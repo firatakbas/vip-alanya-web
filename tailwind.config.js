824f7d81c566f64eb5f864a01edf3f79783f7ffa
firatakbas <firaat.akbas@gmail.com>
2025-05-23 13:33:01 +0300

    tailwind.config.js dosya yolları düzeltilmesi

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
         "./index.html",
         "./tours.html",
         "./cars.html",
+        "./transfert.html",
+        "./rezervision.html",
         "./detail.html",
         "./src/**/*.{js,ts,jsx,tsx}",
         "./public/layout/**/*.html",     // public/layout içindeki tüm HTML dosyalarını tara
